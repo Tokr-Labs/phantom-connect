b51e95f045bb1d97f007c9b28c2978753fbcf03e
Eric McGary <eric@rhove.com>
2022-06-30 13:17:12 -0400

    update to os versions

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PhantomConnect",
-    platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v13),
-    ],
+    platforms: [.iOS(.v11), .macOS(.v10_12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
