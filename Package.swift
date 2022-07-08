06a00f72779741eb882293886e206c16ea02400d
Eric McGary <mcgaryes@gmail.com>
2022-07-08 09:19:58 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PhantomConnect",
-    platforms: [.iOS(.v11),
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "PhantomConnect",
